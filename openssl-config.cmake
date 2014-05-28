7d54e5ae7a91fc0c798fdc461db71c1b2cf6475a
Scott M Anderson <smanders@sdl.usu.edu>
2014-05-28 12:48:09 -0600

    change install destination of -targets.cmake

--- openssl-config.cmake
+++ openssl-config.cmake
@@ -2,8 +2,10 @@
 # OPENSSL_INCLUDE_DIR - the OpenSSL include directory
 # OPENSSL_LIBRARIES - the OpenSSL libraries
 set(prj openssl)
+# this file (-config) installed to share/make (see top-level CMakeLists.txt)
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/${prj}-targets.cmake)
+# targets file (-targets) installed to lib${NUMBITS}/cmake
+include(${SELF_DIR}/../../lib${NUMBITS}/cmake/${prj}-targets.cmake)
 string(TOUPPER ${prj} PRJ)
 get_filename_component(${PRJ}_INCLUDE_DIR "${SELF_DIR}/../../include" ABSOLUTE)
 set(${PRJ}_LIBRARIES crypto ssl)
